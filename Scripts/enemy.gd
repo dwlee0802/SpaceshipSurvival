1914e7e1cfadec9db4138eb6e4ef6eef503d5ad3
unknown <dwlee0802@gmail.com>
2023-11-18 20:02:29 +0900

    Made placeholder hit animation.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,11 +2,21 @@ extends "res://Scripts/unit.gd"
 
 @onready var bodySprite = $BodySprite
 
+var holder: float = 0
+
 func _process(delta):
 	if attackTarget == null:
 		attackTarget = Game.survivors.pick_random()
-		target_position = attackTarget.position
-
+		
+	target_position = attackTarget.position
+	
+	if holder > 0:
+		bodySprite.modulate = Color.WHITE
+		print(holder)
+	else:
+		bodySprite.modulate = Color.INDIAN_RED
+	
+	holder -= delta
 
 func OnDeath():
 	print("Dead!")
@@ -15,3 +25,4 @@ func OnDeath():
 
 func ReceiveHit(amount):
 	super.ReceiveHit(amount)
+	holder = 0.1
