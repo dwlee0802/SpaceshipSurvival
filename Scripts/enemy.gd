ef0c31d2612ec5efb822db48807fa532b1b18b74
unknown <dwlee0802@gmail.com>
2023-11-18 16:07:55 +0900

    Enemies now pick a random survivor to attack.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,3 +1,7 @@
 extends "res://Scripts/unit.gd"
 
 
+func _process(delta):
+	if attackTarget == null:
+		attackTarget = Game.survivors.pick_random()
+		target_position = attackTarget.position
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -9,7 +9,8 @@ static var time: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	survivors.append($Survivor)
+	survivors.append($Survivor2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
