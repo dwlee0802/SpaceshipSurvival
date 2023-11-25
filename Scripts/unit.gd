c7b0637bef176318d99d3d9edb9695d73ac8a4b7
unknown <dwlee0802@gmail.com>
2023-11-25 14:12:44 +0900

    Enhanced movement by selectively using navigation.

--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -2,37 +2,52 @@ extends CharacterBody2D
 
 class_name Unit
 
-@export var health: int = 1
+@export var health: int = 100
 @export var speed: int = 500
 
 var attackTarget
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
+
 @onready var target_position: Vector2 = position
+
+@onready var navRaycast = $NavigationRaycasts/RayCast2D
+@onready var navRaycast2 = $NavigationRaycasts/RayCast2D2
+@onready var navRaycast3 = $NavigationRaycasts/RayCast2D3
+@onready var navRaycast4 = $NavigationRaycasts/RayCast2D4
+@onready var navRaycast5 = $NavigationRaycasts/RayCast2D5
+
+@onready var navUpdateTimer = $NavigationUpdateTimer
 
 var isMoving: bool = false
 
 
 func _physics_process(delta):
-	if nav.is_navigation_finished():
-		isMoving = false
-		return
-	
-	isMoving = true
-		
-	var dir = Vector2()
-	
-	dir = nav.get_next_path_position() - global_position
-	dir = dir.normalized()
-	
-	velocity = velocity.lerp(dir * speed, 1)
-	
-	if nav.is_target_reachable():
-		move_and_slide()
+	if isMoving:
+		if CheckDirectPath():
+			if position.distance_to(target_position) < 5:
+				isMoving = false
+				return
+				
+			print("path clear")
+			velocity = position.direction_to(target_position) * speed
+			move_and_slide()
+		else:
+			print("nav")
+			
+			if nav.is_navigation_finished():
+				isMoving = false
+				return
+				
+			var dir = Vector2()
+			velocity = position.direction_to(nav.get_next_path_position()) * speed
+			move_and_slide()
 
 
 func ChangeTargetPosition(pos):
-	nav.target_position = pos
+	target_position = pos
+	isMoving = true
+	nav.target_position = target_position
 
 
 func ReceiveHit(amount):
@@ -44,3 +59,19 @@ func ReceiveHit(amount):
 
 func OnDeath():
 	print("Dead!")
+	
+	
+func CheckDirectPath():
+	var dir = position.direction_to(target_position)
+	var dist = position.distance_to(target_position)
+	
+	navRaycast.target_position = dir * dist
+	navRaycast2.target_position = dir * dist
+	navRaycast3.target_position = dir * dist
+	navRaycast4.target_position = dir * dist
+	navRaycast5.target_position = dir * dist
+	
+	if navRaycast.get_collider() == null and navRaycast2.get_collider() == null and navRaycast3.get_collider() == null and navRaycast4.get_collider() == null and navRaycast5.get_collider() == null:
+		return true
+	else:
+		return false
