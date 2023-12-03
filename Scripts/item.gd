e14e71c25cc25797a7f0f20c7de19aa49731adb7
unknown <dwlee0802@gmail.com>
2023-12-03 14:52:57 +0900

    Added UI elements for interacting with containers.

---
+++ Scripts/item.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var type: int = -1
+var data
+static var itemDict
+
+func _init(_type, _id):
+	type = _type
+	
+	if type == ItemType.Melee:
+		data = itemDict.Melee[_id]
+	elif type == ItemType.Ranged:
+		data = itemDict.Ranged[_id]
+	elif type == ItemType.Head:
+		data = itemDict.Head[_id]
+	elif type == ItemType.Body:
+		data = itemDict.Body[_id]
+	elif type == ItemType.Consumable:
+		data = itemDict.Consumable[_id]
+
+func _to_string():
+	var output = data.name + "\n"
+	output += "Type: " + str(type) + "\nID: " + str(data.ID)
+	return output
