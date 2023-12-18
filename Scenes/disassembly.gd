ffb58a028f3cfbcd7d62bf047fe1480a943ac18e
unknown <dwlee0802@gmail.com>
2023-12-18 22:23:04 +0900

    Disassembly UI only shows when a survivor is interacting with it.

--- Scenes/disassembly.gd
+++ Scenes/disassembly.gd
@@ -3,8 +3,19 @@ class_name Disassembly
 
 var isOperational: bool = true
 
+var showUI: bool = false
+
+
+func _process(delta):
+	if showUI:
+		UserInterfaceManager.disassemblyUI.visible = true
+	else:
+		UserInterfaceManager.disassemblyUI.visible = false
+		
+	showUI = false
+	
 
 func Fix(delta):
-	super.Fix(delta)
 	# show disassembly UI
-	UserInterfaceManager.disassemblyUI.visible = true
+	showUI = true
+	return false
