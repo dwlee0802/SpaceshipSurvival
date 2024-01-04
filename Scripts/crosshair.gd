d8005dbe72f92b2b027389a84120f95fa60955a8
unknown <dwlee0802@gmail.com>
2024-01-04 23:29:01 +0900

    Crosshair changes size with spread. Needs improvement.

--- Scripts/crosshair.gd
+++ Scripts/crosshair.gd
@@ -1,8 +1,22 @@
 extends Control
 
 @onready var label = $AmmoLabel
+@onready var image = $TextureRect
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = get_global_mouse_position()
 	label.text = str(Game.survivor.magazineCount)
+	ApplySpread()
+
+
+# increase size based on survivor's spread
+# base size is 50. increase to 150
+# size is 50 at 0 spread and 150 at 180 spread
+func ApplySpread():
+	var survivor = Game.survivor
+	var newSize = 50 + 100 * survivor.spread * 57 / 45
+	image.size = Vector2(newSize, newSize)
+	print(newSize)
+	# center image
+	image.position = Vector2(-newSize / 2, -newSize / 2)
