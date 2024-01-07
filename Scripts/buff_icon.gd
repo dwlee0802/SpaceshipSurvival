8693b8e6ee03347b2482953eddbc7d9ba135e20b
unknown <dwlee0802@gmail.com>
2024-01-07 20:17:37 +0900

    Buff icons now show duration.

--- Scripts/buff_icon.gd
+++ Scripts/buff_icon.gd
@@ -4,7 +4,14 @@ class_name BuffIcon
 var data: BuffSkill
 @onready var timer: Timer = $BuffDurationTimer
 
+@onready var cooldownIcon = $Cooldown
 
+
+func _process(delta):
+	cooldownIcon.size = Vector2(size.x, timer.time_left / timer.wait_time * size.x)
+	cooldownIcon.position = size
+	
+	
 func SetData(skillData: BuffSkill):
 	data = skillData
 	texture = data.texture
