490f17dee79f62eff1801209fa399f6733d681e1
unknown <dwlee0802@gmail.com>
2024-01-26 23:18:11 +0900

    Added references to tree UIs.

--- Scenes/WeaponUpgradeUI.gd
+++ Scenes/WeaponUpgradeUI.gd
@@ -1,5 +1,9 @@
 extends "res://Scripts/draggable_window.gd"
+class_name WeaponUpgradeUI
 
+@onready var tree_1 = $Tree_1
+@onready var tree_2 = $Tree_2
+@onready var tree_3 = $Tree_3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
